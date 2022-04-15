708b59e1b71697d91ac50109769ab9e0df77f125
huzaifa <huzaifashaikh741@gmai.com>
2022-04-15 14:58:06 +0500

    update

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -218,7 +218,7 @@ class _HomeState extends State<HomeScreen> {
             new UserAccountsDrawerHeader(
                 currentAccountPicture:
                     Image.asset('assets/images/app_icon.png'),
-                accountName: new Text('SMS Blocker Pro'),
+                accountName: new Text('Spam Blocker Pro'),
                 accountEmail: null),
             new Column(children: drawerOptions)
           ],
